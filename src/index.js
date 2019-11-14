0ab0ecd39e346c325a8988f8935179e1c8c0e9a7
Okafor Irene <okaforirenen@gmail.com>
2019-11-14 08:31:21 +0100

    added scale transformation to nav items on hover. removed nav scrollbar on screens >=1200px

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom";
+import {applyMiddleware , compose , combineReducers , createStore } from 'redux';
+import { Provider } from "react-redux";
+import thunk from 'redux-thunk';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const app = <Router>
-    <App />
-</Router>;
+const composeEnhancers = process.env.NODE_ENV === "development" ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+
+const rootReducer = combineReducers(
+    {
+       
+    }
+);
+
+const appStore = createStore(rootReducer,composeEnhancers( applyMiddleware(thunk) ));
+
+const app = (
+    <Provider store={appStore}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>
+);
+
 
 ReactDOM.render(app, document.getElementById('root'));
 

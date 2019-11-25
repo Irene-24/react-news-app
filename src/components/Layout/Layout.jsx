cc5b333828bbf4ac9bd610a02f37e8734ebb8749
Okafor Irene <okaforirenen@gmail.com>
2019-11-25 14:14:52 +0100

    removed unnessary class in layout component

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,41 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 import Sidebar from "../SideBar/SideBar";
 
-class Layout extends Component 
-{
-    constructor(props)
-    {
-      super(props);
-      this.state =
-      {
-       showSideDrawer:false,
-      }
-    }
+const layout = props => {
+  return (
+    <>
+      <Sidebar />
+      {props.children}
+    </>
+  );
+};
 
-    componentDidMount ()
-    {
-
-    }
-
-    render()
-    { 
-        return(
-              
-
-            <> 
-                <Sidebar/> 
-                {this.props.children} 
-            
-            </>
-
-                
-            )
-    }
-}
-
-    ;
-
-
-export default Layout;
-
-
+export default layout;

0eb6d4732677dbf60248c1772a72e95f59515f0c
Okafor Irene <okaforirenen@gmail.com>
2019-11-28 17:28:45 +0100

    removed scroll stuff

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom";
-import { createBrowserHistory } from "history";
+
 import {applyMiddleware , compose , combineReducers , createStore } from 'redux';
 import { Provider } from "react-redux";
 import thunk from 'redux-thunk';
@@ -21,11 +21,12 @@ const rootReducer = combineReducers(
 );
 
 
+
 const appStore = createStore(rootReducer,composeEnhancers( applyMiddleware(thunk) ));
 
 const newsApp = (
     <Provider store={appStore}>
-        <Router onUpdate={() => window.scrollTo(0, 0)} history={createBrowserHistory()}>
+        <Router >
             <App />
         </Router>
     </Provider>

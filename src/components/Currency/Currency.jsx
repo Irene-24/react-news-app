f899725abb83d0bd39eba3337b0dc0da5c7c58ca
Okafor Irene <okaforirenen@gmail.com>
2019-12-18 13:49:08 +0100

    forced convert buton to be disabled if no source and target

--- src/components/Currency/Currency.jsx
+++ src/components/Currency/Currency.jsx
@@ -1,99 +1,117 @@
-import React  from "react";
+import React from "react";
 import convert from "../../assets/img/convert.svg";
 import convert_hori from "../../assets/img/convert_hori.svg";
 import classes from "./Currency.module.css";
 
-const currency = (props) => 
-{
-    const options = [];
-    // eslint-disable-next-line no-unused-vars
-    for (const countryCode in props.options) 
-    {
-        const option = { countryCode,...props.options[countryCode] };
-        options.push(option);
-    }
+const currency = props => {
+  const options = [];
+  // eslint-disable-next-line no-unused-vars
+  for (const countryCode in props.options) {
+    const option = { countryCode, ...props.options[countryCode] };
+    options.push(option);
+  }
 
-    options.sort((a,b) => a.currencyName > b.currencyName ?1 : -1);   
+  options.sort((a, b) => (a.currencyName > b.currencyName ? 1 : -1));
 
+  return (
+    <div className={classes.Converter}>
+      <p className={classes.Converter_Header}>Currency Converter</p>
 
-    return (
-      <div className={classes.Converter}>
-        <p className={classes.Converter_Header}>Currency Converter</p>
+      <div className={classes.Converter_Grid}>
+        <div className={classes.Inputs}>
+          <label htmlFor="source"> Select source </label>
 
-        <div className={classes.Converter_Grid}>
-          <div className={classes.Inputs}>
-            <label htmlFor="source"> Select source </label>
+          <select onChange={props.pickCurrency} id="source">
+            <option value=""> Select source currency</option>
+            {options.map(opt => {
+              return (
+                <option key={opt.currencyName} value={opt.countryCode}>
+                  {" "}
+                  {opt.currencyName} {`( ${opt.currencySymbol} )`}
+                </option>
+              );
+            })}
+          </select>
 
-            <select onChange={props.pickCurrency} id="source">
-              <option value=""> Select source currency</option>
-              {
-                options.map( opt => 
-                    {
-                return <option key={opt.currencyName} value={opt.countryCode}> {opt.currencyName}  { `( ${opt.currencySymbol} )` }</option> 
-                    } )
-              }
+          <label htmlFor="target"> Select target </label>
 
-            </select>
+          <select onChange={props.pickCurrency} id="target">
+            <option value="">Select target currency</option>
 
-            <label htmlFor="target"> Select target </label>
+            {options.map(opt => {
+              return (
+                <option key={opt.currencyName} value={opt.countryCode}>
+                  {" "}
+                  {opt.currencyName} {`( ${opt.currencySymbol} )`}
+                </option>
+              );
+            })}
+          </select>
 
-            <select onChange={props.pickCurrency} id="target">
-              <option value="">Select target currency</option>
+          <label htmlFor="amt"> Enter amount </label>
 
-              {
-                options.map( opt => 
-                    {
-                return <option key={opt.currencyName} value={opt.countryCode}> {opt.currencyName}  { `( ${opt.currencySymbol} )` }</option> 
-                    } )
-              }
-             
-            </select>
+          <input
+            value={props.amount}
+            onChange={props.inputHandler}
+            onKeyUp={props.pressedEnter}
+            type="number"
+            id="amt"
+          />
 
-            <label htmlFor="amt"> Enter amount </label>
+          {props.source && props.target ? (
+            <button
+              onClick={props.convertCurrency}
+              className={classes.Convert_Btn}
+            >
+              Convert
+            </button>
+          ) : (
+            <button
+              disabled
+              onClick={props.convertCurrency}
+              className={classes.Convert_Btn}
+            >
+              Convert
+            </button>
+          )}
+          
+        </div>
+        <div className={classes.Display}>
+          <div className={classes.Currency}>
+            <p className={classes.Value}>{props.sourceVal}</p>
+            <p className={classes.Symbol}>
+              {props.source !== ""
+                ? props.options[props.source].currencySymbol
+                : "#"}
+            </p>
+          </div>
 
-            <input
-              value={props.amount}
-              onChange={props.inputHandler}
-              onKeyUp={props.pressedEnter}
-              type="number"
-              id="amt"
+          <div className={classes.Convert_Icon}>
+            <img
+              className={classes.Vert}
+              src={convert}
+              alt="two vertical arrows icon"
             />
 
-            <button onClick = {props.convertCurrency} className={classes.Convert_Btn}>Convert</button>
+            <img
+              className={classes.Hori}
+              src={convert_hori}
+              alt="two horizontal arrows icon"
+            />
           </div>
-          <div className={classes.Display}>
 
-            <div className={classes.Currency}>
-              <p className={classes.Value}>{props.sourceVal}</p>
-              <p className={classes.Symbol}>
-              {
-                      props.source !== "" ? 
-                      props.options[props.source].currencySymbol : '#'
-                  }
-              </p>
-            </div>
-
-            <div className={classes.Convert_Icon}>
-              <img className={classes.Vert} src={convert} alt="two vertical arrows icon" />
-              
-              <img className={classes.Hori} src={convert_hori} alt="two horizontal arrows icon" />
-            </div>
-
-            <div className={classes.Currency}>
-              <p className={classes.Value}>{props.targetVal}</p>
-              <p className={classes.Symbol}>
-                  {
-                      props.target !== "" ? 
-                      props.options[props.target].currencySymbol : '#'
-                  }
-              </p>
-            </div>
-
+          <div className={classes.Currency}>
+            <p className={classes.Value}>{props.targetVal}</p>
+            <p className={classes.Symbol}>
+              {props.target !== ""
+                ? props.options[props.target].currencySymbol
+                : "#"}
+            </p>
           </div>
         </div>
       </div>
-    );
-}
-
+    </div>
+  );
+};
 
 export default currency;
